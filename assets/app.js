9d1ba7c11d76018ebe64dc271f9f18c7f73b04f8
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2021-12-17 14:05:35 +0000

    added mobile navigation menu in html and css, added javascript to toggle menu visibility and button interactivity

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,7 @@
 const menuBtn = document.querySelector('.hamburger');
+const navContainer = document.querySelector('.nav-container');
 
 menuBtn.addEventListener('click', function() {
     menuBtn.classList.toggle('active');
+    navContainer.classList.toggle('visible');
 })
